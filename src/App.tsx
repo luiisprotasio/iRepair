2c945a15ca3b7ee0704009350a306a949a9a5ff6
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-09 19:33:44 -0300

    feat: adiciona container de nova ordem

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import { Header } from "./components/Header.tsx";
 import { CardList } from "./components/CardList.tsx";
+import { NewOrderForm } from "./components/NewOrderForm.tsx";
+
 function App() {
   return (
     <div className="bg-radial from-[#133036] to-[#0a181b] min-h-screen">
       <Header />
-      <div className="flex justify-around">
+      <div className="flex justify-start">
           <CardList />
+          <NewOrderForm />
       </div>
     </div>
   )
