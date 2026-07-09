a7a76279eb4908a0a031c4fd2b6267c50ce26e5c
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-09 19:34:26 -0300

    refactor: reestiliza as abas da interface

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,6 +1,6 @@
 import "../App.css";
 export function CardList() {return(
-<div className=" ml-10 w-60/100 h-220">
+<div className=" ml-10 w-70/100 h-220">
     <h1 className="m-8 text-3xl text-white">Ordens de serviço</h1>
     <div className="m-3 flex justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full h-200 overflow-x-auto overflow-y-hidden">
     </div>
--- src/components/NewOrderForm.tsx
+++ src/components/NewOrderForm.tsx
@@ -1,6 +1,6 @@
 import "../App.css";
 export function NewOrderForm() {return(
-<div className=" ml-10 w-30/100 h-220">
+<div className=" ml-10 w-20/100 h-220">
     <h1 className="m-8 text-3xl text-white">Nova Ordem</h1>
     <div className="m-3 flex justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full h-200 overflow-x-auto overflow-y-hidden">
     </div>
