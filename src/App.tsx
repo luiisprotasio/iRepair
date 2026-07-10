196ed5e313087e8950f7861f6ddbf6d7298dd10c
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-10 13:31:02 -0300

    feat: adiciona template de card incompleto

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 import { Header } from "./components/Header.tsx";
 import { CardList } from "./components/CardList.tsx";
 import { NewOrderForm } from "./components/NewOrderForm.tsx";
+import type { ServiceOrder } from "./types/ServiceOrders.tsx";
+import { OrderCard } from "./components/OrderCard.tsx";
 
 function App() {
+ 
   return (
     <div className="bg-radial from-[#133036] to-[#0a181b] min-h-screen">
       <Header />
       <div className="flex justify-start">
           <CardList />
           <NewOrderForm />
+          <OrderCard />
       </div>
     </div>
   )
