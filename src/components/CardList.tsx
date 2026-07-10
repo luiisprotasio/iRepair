e5d634758134d74000b04cf573b8370bb7f38512
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-10 17:34:32 -0300

    fix: corrige a renderização dos cards

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -8,7 +8,7 @@ interface CardListProps {
 export function CardList({ orders, onToggleStatus }: CardListProps) {return(
 <div className=" ml-10 w-70/100 h-220">
     <h1 className="m-8 text-3xl text-white">Ordens de serviço</h1>
-    <div className=" p-3 gap-3 m-3 flex justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full h-200 overflow-y-auto">{orders.map((currentOrder) => (
+    <div className=" p-3 gap-3 m-3 flex flex-wrap justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full min-h-[200px] overflow-y-auto">{orders.map((currentOrder) => (
         <OrderCard key={currentOrder.id} order={currentOrder} onToggleStatus={onToggleStatus}/>
     ))}
     </div>
--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -7,7 +7,7 @@
     }
     export function OrderCard({ order, onToggleStatus }: OrderCardProps){
         return (
-            <div className="m-3 flex-col justify-start rounded-3xl bg-[#301e1f]/70 border border-[#653a3a] w-14/100 h-75 overflow-x-auto overflow-y-hidden p-2">
+            <div className="m-3 flex-col justify-start rounded-3xl bg-[#301e1f]/70 border border-[#653a3a] w-56 h-75 p-2">
                 <div className="border border-[#653a3a] rounded-lg text-white text-sm h-20/100 overflow-x-auto overflow-y-auto">
                 <h2>Cliente:</h2>
                 <h3 id="clientName">{order.client}</h3>
