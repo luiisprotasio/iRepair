ea31aafceb785a0ef4a2a9f5456bf567a770a992
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-10 17:28:59 -0300

    fix: conserta checagem da checkbox de conclusão

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -5,11 +5,11 @@ interface CardListProps {
   orders: ServiceOrder[];
   onToggleStatus: (id: number) => void;
 }
-export function CardList({ orders }: CardListProps) {return(
+export function CardList({ orders, onToggleStatus }: CardListProps) {return(
 <div className=" ml-10 w-70/100 h-220">
     <h1 className="m-8 text-3xl text-white">Ordens de serviço</h1>
-    <div className=" p-3 gap-3 m-3 flex justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full h-200 overflow-x-auto overflow-y-hidden">{orders.map((currentOrder) => (
-        <OrderCard key={currentOrder.id} order={currentOrder} />
+    <div className=" p-3 gap-3 m-3 flex justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full h-200 overflow-y-auto">{orders.map((currentOrder) => (
+        <OrderCard key={currentOrder.id} order={currentOrder} onToggleStatus={onToggleStatus}/>
     ))}
     </div>
 </div>
--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -3,8 +3,9 @@
     import { useState } from "react";
     interface OrderCardProps{
         order: ServiceOrder
+        onToggleStatus: (id: number) => void;
     }
-    export function OrderCard({order}:OrderCardProps) {
+    export function OrderCard({ order, onToggleStatus }: OrderCardProps){
         return (
             <div className="m-3 flex-col justify-start rounded-3xl bg-[#301e1f]/70 border border-[#653a3a] w-14/100 h-75 overflow-x-auto overflow-y-hidden p-2">
                 <div className="border border-[#653a3a] rounded-lg text-white text-sm h-20/100 overflow-x-auto overflow-y-auto">
@@ -25,7 +26,7 @@
                 <h2>Prazo de entrega:</h2>
                 <time id="cardTime"dateTime={order.date}></time>
                 </div>
-                <div className="text-white"> <input type="checkbox" name="Concluido" id="checkCard" checked={order.status} />Conclúido</div>
+                <div className="text-white"> <input type="checkbox" name="Concluido" id="checkCard" onChange={() => onToggleStatus(order.id)} checked={order.status} />Concluído</div>
                
             </div>
         )
