89396f27c665cec63c0a57c2859877ac1ddfbd18
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-17 16:56:03 -0300

    refactor: reestiliza o dashboard

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -9,8 +9,8 @@ interface CardListProps {
   onDeleteOrder: (id: number) => void;
 }
 export function CardList({ orders, clients, onChangeStatus ,onDeleteOrder}: CardListProps) {return(
-<div className=" ml-10 w-70/100 h-220">
-    <h1 className="m-8 text-3xl text-white">Ordens de serviço</h1>
+<div className=" ml-10 w-95/100 h-220">
+    <h1 className="m-8 text-3xl text-white text-center">Ordens de serviço</h1>
     <div className=" p-3 gap-3 m-3 flex flex-wrap justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full min-h-100 max-h-200 overflow-y-auto">{orders.map((currentOrder) =>{
         const clienteDoCard = clients.find(c => c.id === currentOrder.client_id); 
         const name = clienteDoCard?.name || "Cliente não encontrado";
