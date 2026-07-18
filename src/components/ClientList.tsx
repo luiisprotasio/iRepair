11ebb29588d269e3ead406adfc5f11e5f0dfd24a
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-07-18 15:57:47 -0300

    feat: adiciona tela de ordens de serviço e form pra criar nova ordem

--- src/components/ClientList.tsx
+++ src/components/ClientList.tsx
@@ -12,7 +12,7 @@ export function ClientList({clients,onDeleteClient}:ClientListProps){
            <div className=" p-3 gap-3 m-3 flex flex-wrap justify-start rounded-xl bg-[#12282d]/70 border border-[#3a5d65] w-full min-h-100 max-h-200 overflow-y-auto">{clients.map((currentClient) =>{
                return (
                <ClientCard key={currentClient.id} client={currentClient} onDeleteClient={onDeleteClient}/>
-           )})}
+           )})} 
            </div>
        </div>
     )
