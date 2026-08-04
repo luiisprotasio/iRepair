e4ce5ab2fbd43c452a886759dfad6c98e91d6072
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-04 11:50:09 -0300

    fix: corrige instalação do adapter

--- api/src/config/expressConfig.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { tasksApp } from '../domains/temp/routes/TarefaRoutes';
-const app = express();
-app.use(express.json());
-app.use('/tasks', tasksApp);
-export { app };
--- api/src/server.js
+++
@@ -1,4 +0,0 @@
-import { app } from './config/expressConfig';
-app.listen(1412, () => {
-    console.log('🚀 Servidor rodando na porta 1412');
-});
