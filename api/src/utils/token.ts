c69fb56a53757949ccd44a2178a77725ccb20130
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-06 18:35:40 -0300

    feat: adiciona os métodos de token JWT

---
+++ api/src/utils/token.ts
@@ -0,0 +1,18 @@
+import jwt, { SignOptions } from 'jsonwebtoken'
+
+interface TokenPayload {
+  id: number
+  email: string
+  iat: number
+  exp: number
+}
+
+export function verifyToken(token: string): TokenPayload {
+  // jwt.verify lança uma exceção se o token for inválido ou expirado
+  const decoded = jwt.verify(token, process.env.JWT_SECRET) as TokenPayload
+  return decoded
+}
+export function generateToken(payload: TokenPayload): string {
+  return jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN as SignOptions['expiresIn'],
+  })}
