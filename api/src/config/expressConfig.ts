d554c44cf6089a29bdbaa7f782872dd9d9349761
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-06 19:19:47 -0300

    feat: implementa as rotas de autenticação

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { tasksApp } from '../domains/temp/routes/TarefaRoutes';
+import { authRoutes } from '../domains/auths/auth.routes';
 
 const app = express();
 app.use(express.json());
 app.use('/tasks', tasksApp);
-
+app.use('/auth', authRoutes);
 export { app };
