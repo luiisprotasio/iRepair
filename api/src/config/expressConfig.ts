622d93fec1a87d66505a929a9208ae3320b656aa
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-08 09:07:30 -0300

    chore: configura dependencias restantes

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { tasksApp } from '../domains/temp/routes/TarefaRoutes';
 import { authRoutes } from '../domains/auths/auth.routes';
-
+import cookieParser from 'cookie-parser'
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use('/tasks', tasksApp);
 app.use('/auth', authRoutes);
