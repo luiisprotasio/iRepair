ce3741aeab24534025d00bc6b98c1ecbf690967c
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-12 10:28:12 -0300

    feat: adiciona menu de perfil para controle de login

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,6 @@
     import "../App.css";
     import { Link } from 'react-router';
+    import { Menu } from "./Menu";
     export const Header=() =>{
         return (
             <header className="flex justify-between items-center bg-linear-90 from-[#0a181b] via-[#133036] to-[#0a181b] border-b border-[#3a5d65]  p-4">
@@ -7,10 +8,11 @@
             <h1 className="text-2xl text-transparent bg-clip-text bg-linear-90 from-[#42C0DF] to-[#9dd5e2] font-bold">iRepair</h1>
             </div>
             <nav className="flex gap-7 text-white text-lg    m-2 ">
+                <Menu />
                 <Link className="hover:text-blue-300"to="/">Dashboard</Link>
                 <Link className="hover:text-blue-300" to="/clients">Clientes</Link>
                 <Link className="hover:text-blue-300" to="/service-orders">Ordens de Serviço</Link>
-                <Link className="hover:text-red-700" to="/logout">Sair</Link>
+               
             </nav>
             </header>
         )
