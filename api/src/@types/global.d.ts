07693c9472d28a5f7f9dc497b10c9d79b230865f
luiisprotasio <luisfeliperochaprotasio@gmail.com>
2026-08-12 08:29:35 -0300

    feat: adiciona as configurações de envvar do refresh token

--- api/src/@types/global.d.ts
+++ api/src/@types/global.d.ts
@@ -7,6 +7,8 @@ declare global {
       DATABASE_URL: string
       JWT_SECRET: string
       JWT_EXPIRES_IN: string
+      JWT_REFRESH_SECRET: string
+      JWT_REFRESH_EXPIRES_IN: string
       NODE_ENV: 'development' | 'production' | 'test'
     }
   }
